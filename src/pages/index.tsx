21c01179a2282d24ec0d073a3bf81d43e047de2a
Rafael Pinto <archlinuxrp@gmail.com>
2022-11-27 14:16:13 +0000

    remove clock from get started btn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Get Started ⏱️
+            Get Started
           </Link>
         </div>
       </div>
